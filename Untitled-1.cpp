963cc5a483f0756c103679a568cc9fbbe4c3dfd6
bubelnyk_yura <urabubelnik@gmail.com>
2024-09-28 17:56:13 +0300

    Hello + I

---
+++ Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7,b = 6;
+    
+    if (a > b){
+        cout << "Hello Lviv Polytechnic";
+        a += b;
+    }else{
+        cout << "Hello World + I";
+    }
+
+    return 0;
+}
